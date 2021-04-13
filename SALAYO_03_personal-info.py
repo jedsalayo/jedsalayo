e6c317d3d60e9e5f91cbffc51f22f9beced4d832
jedsalayo <82429228+jedsalayo@users.noreply.github.com>
2021-04-13 12:02:09 +0800

    Add files via upload

---
+++ SALAYO_03_personal-info.py
@@ -0,0 +1,6 @@
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+yourLocation = input ("What is your location? ")
+yourAge = input ("What is your age? ")
+space = " "
+print("Hi " + firstName + space + lastName + "!" + space + "Your location is " + yourLocation + " and you are " + yourAge + " years old")
